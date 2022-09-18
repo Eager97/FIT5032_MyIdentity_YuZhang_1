008e07fe22855128025925e33b1329e47a2a5980
YuZhang <yuzhang1997@163.com>
2022-09-18 21:32:06 +1000

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_MyIdentity_YuZhang.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_MyIdentity_YuZhang.Startup))]
+namespace FIT5032_MyIdentity_YuZhang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
